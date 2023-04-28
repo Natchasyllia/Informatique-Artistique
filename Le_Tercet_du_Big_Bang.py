6a37d6531d7b76e55a7bb9605e7147fea764757b
Natchasyllia <114140811+Natchasyllia@users.noreply.github.com>
2023-04-28 14:24:53 +0200

    Add files via upload
    Pour i allant de un à n égal à quatre, 
    Hawking, Gamow, Einstein, Friedman avec Lemaître,
    Prions le Saint-Esprit, dans le fils et le père !

---
+++ Le_Tercet_du_Big_Bang.py
@@ -0,0 +1,50 @@
+# Le Tercet du Big Bang
+# Jiaxi Xu
+# 27 Avril 2023
+
+'''
+Pour i allant de un à n égal à quatre, 
+Hawking, Gamow, Einstein, Friedman avec Lemaître,
+Prions le Saint-Esprit, dans le fils et le père !
+'''
+
+import sys, os
+
+def Hawking():
+    os.fork()
+    print("Viens Esprit de feu allumer ta flamme dans le cœur de tes fidèles.")
+
+def Gamow():
+    os.fork()
+    print("Viens Esprit de force donner courage pour les oeuvres de charité.")
+
+def Einstein():
+    os.fork()
+    print("Viens Esprit de sagesse conduire nos existences dans l'audace et la prudence.")
+
+def Friedman():
+    os.fork()
+    print("Viens Esprit de conseil éclairer le chemin vers Dieu notre Père.")
+
+def Lemaitre():
+    os.fork()
+    print("Au nom du Père et du Fils et du Saint-Esprit. Amen.")
+
+def QActus():
+    os.fork()
+    print("Qui sont ces satanistes qui veulent détruire l'humanité par le wokisme?")
+    print("Pédophilie: Ce que savait Lady Diana...")
+
+def BigBang():
+    for i in range(1,5):
+        Gamow()
+        Einstein()
+        Friedman()
+        Lemaitre()
+        #sys.exit(0)
+        BigBang()
+
+BigBang()
+
+# A recopier dans le Terminal :
+# python3 Le_Tercet_du_Big_Bang.py
